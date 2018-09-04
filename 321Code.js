dd46b00c32992bf851012359e0e43aea556afc77
Rosse <rhaneyrrn@gmail.com>
2018-09-04 16:58:48 -0500

    Commit files

---
+++ 321Code.js
@@ -0,0 +1,4 @@
+console.log("Rose 20");
+console.log(4 + 5);
+console.log("4 + 5");
+console.log("4" + "5");
---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Rose 20");
+console.log(4 + 5);
+console.log("4 + 5");
+console.log("4" + "5");
+
+
+const dif = 32;
+//let input = prompt("Enter a the temperature in Celcius:"); 
+//let tempC = Number(input);
+
+
+let tempC = 40;
+let tempF = (tempC * (9/5)) + dif;
+
+console.log(tempF);
